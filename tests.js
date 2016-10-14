ae67ee59654b199d117ef369be6159fd3d3caa6b
Olawale Akinseye <brainyfarm@gmail.com>
2016-10-14 18:16:41 +0100

    Refractored tests.js

--- tests.js
+++ tests.js
@@ -5,53 +5,53 @@ var assert = chai.assert;
 
 var lib = require('./lib/sum_of_primes.js');
 
-describe("Verify that the sum of prime is accurate ", function() {
-  
-  it("Should return 129 for sum_of_primes(30)", function() {
+describe("Verify that the sum of prime is accurate ", function () {
+
+  it("Should return 129 for sum_of_primes(30)", function () {
     assert(
       lib.sum_of_primes(30) === 129
     );
   });
-  
-  it("Should return 0 for sum_of_primes(0)", function() {
+
+  it("Should return 0 for sum_of_primes(0)", function () {
     assert(
       lib.sum_of_primes(0) === 0
     );
   });
 
-    it("Should return 5 for sum_of_primes(3)", function() {
+  it("Should return 5 for sum_of_primes(3)", function () {
     assert(
       lib.sum_of_primes(3) === 5
     );
   });
 
-    it("Should return 17 for sum_of_primes(9)", function() {
+  it("Should return 17 for sum_of_primes(9)", function () {
     assert(
       lib.sum_of_primes(9) === 17
     );
   });
 
-    it("Should return 28 for sum_of_primes(11)", function() {
+  it("Should return 28 for sum_of_primes(11)", function () {
     assert(
       lib.sum_of_primes(11) === 28
     );
   });
 
-    it("Should return 0 for negative inputs", function() {
+  it("Should return 0 for negative inputs", function () {
     assert(
       lib.sum_of_primes(-7) === 0
     );
   });
 
-    it("Should return 0 for string inputs", function() {
+  it("Should return 0 for string inputs", function () {
     assert(
       lib.sum_of_primes("") === 0
     );
   });
 
 
-  
-    it("Should return 0 for string inputs", function() {
+
+  it("Should return 0 for string inputs", function () {
     assert(
       lib.sum_of_primes("777") === 0
     );
@@ -61,15 +61,15 @@ describe("Verify that the sum of prime is accurate ", function() {
 
   });
 
-  
-    it("Should return 0 for sum_of_primes(1)", function() {
+
+  it("Should return 0 for sum_of_primes(1)", function () {
     assert(
       lib.sum_of_primes(1) === 0
     );
   });
 
-  
-    it("Should return 0 for sum_of_primes([5,22])", function() {
+
+  it("Should return 0 for sum_of_primes([5,22])", function () {
     assert(
       lib.sum_of_primes([5, 22]) === 0
     );
