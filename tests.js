3681515a33b0ac39debfdb22c192ec79020cc1bd
Olawale Akinseye <brainyfarm@gmail.com>
2016-10-17 10:33:03 +0100

    Fixed test errors

--- tests.js
+++ tests.js
@@ -9,65 +9,65 @@ var testSumOfPrimes = lib.sumOfPrimes;
 describe("Verify that the sum of primes are accurate ", function () {
 
   it("Should return 129 for 30", function () {
-    assert(
+    assert.equal(
       testSumOfPrimes(30), 129
     );
   });
 
   it("Should return 0 for 0", function () {
-    assert(
-      testSumOfPrimes(0) === 0
+    assert.equal(
+      testSumOfPrimes(0), 0
     );
   });
 
   it("Should return 5 for 3", function () {
-    assert(
+    assert.equal(
       testSumOfPrimes(3), 5
     );
   });
 
   it("Should return 17 for 9", function () {
-    assert(
+    assert.equal(
       testSumOfPrimes(9), 17
     );
   });
 
   it("Should return 28 for 11", function () {
-    assert(
+    assert.equal(
       testSumOfPrimes(11), 28
     );
   });
 
   it("Should return 0 for negative inputs", function () {
-    assert(
-      testSumOfPrimes(-7) === 0
+    assert.equal(
+      testSumOfPrimes(-7), 0
     );
   });
 
   it("Should return 0 for string inputs", function () {
-    assert(
-      testSumOfPrimes("") === 0
+    assert.equal(
+      testSumOfPrimes(""), 0
     );
   });
 
 
   it("Should return 0 for string inputs", function () {
-    assert(
-      testSumOfPrimes("777") === 0
+    assert.equal(
+      testSumOfPrimes("777"), 0
     );
   });
 
 
   it("Should return 0 for 1", function () {
-    assert(
-      testSumOfPrimes(1) === 0
+    assert.equal(
+      testSumOfPrimes(1), 0
     );
   });
 
 
   it("Should return 0 for [5,22]", function () {
-    assert(
-      testSumOfPrimes([5, 22]) === 0
+    assert.equal(
+      testSumOfPrimes([5, 22]),  0
     );
   });
 
